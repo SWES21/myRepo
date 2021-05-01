70b7a0a4b369f5e3b6fb164f37a90f726948a209
Michael <mhf25@case.edu>
2021-05-01 18:49:04 -0400

    firstDesign

---
+++ Pods/Target Support Files/Pods-myIOSApp/Pods-myIOSApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_myIOSApp : NSObject
+@end
+@implementation PodsDummy_Pods_myIOSApp
+@end
---
+++ myIOSApp/ViewController/loginSignUp/moreInfoPage.swift
@@ -0,0 +1,18 @@
+//
+//  moreInfoPage.swift
+//  myIOSApp
+//
+//  Created by Michael  on 4/25/21.
+//
+
+import UIKit
+
+class moreInfoPage: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
