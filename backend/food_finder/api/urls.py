3d9a792d1dda380736b758f7baf8d00cced69e56
Ford Smith <fms34@case.edu>
2021-05-04 18:36:02 -0400

    Added urls for preference data

--- backend/food_finder/api/urls.py
+++ backend/food_finder/api/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 
 from . import restaurant_data
 from . import user_accounts
+from . import preference_data
 
 urlpatterns = [
     path('restaurant/new', restaurant_data.restaurant_add, name='restaurant-new'),
     path('restaurant/<int:restaurant_id>', restaurant_data.restaurant_detail, name='restaurant-detail'),
 
-    # path('user/get_recommendations'i, )
+    path('user/update_preferences/liked', preference_data.update_preferences_liked),
 
-    path('login/', user_accounts.login_user),
-    path('logout/', user_accounts.logout_user),
-    path('signup/', user_accounts.signup_user)
+    path('login/', user_accounts.login_user, name='login'),
+    path('logout/', user_accounts.logout_user, name='logout'),
+    path('signup/', user_accounts.signup_user, name='signup')
 ]
