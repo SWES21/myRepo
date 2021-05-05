96cc342b8dc5c0331b5313d0bee7e70b50d74824
Ford Smith <fms34@case.edu>
2021-05-05 01:25:29 -0400

    Fixed url

--- backend/food_finder/api/urls.py
+++ backend/food_finder/api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('restaurant/new', restaurant_data.restaurant_add, name='restaurant-new'),
     path('restaurant/<int:restaurant_id>', restaurant_data.restaurant_detail, name='restaurant-detail'),
 
-    path('user/recommendations/update/liked', preference_data.update_preferences_liked, name='update-preferences'),
-    # path('user/recommendation/update/disliked', ),
+    path('user/recommendations/update/liked', preference_data.update_preferences_liked, name='update-pref-like'),
+    path('user/recommendations/update/disliked', preference_data.update_preferences_disliked, name='update-pref-dislike'),
     path('user/recommendations/get', preference_data.get_recommendations),
 
     path('login/', user_accounts.login_user, name='login'),
