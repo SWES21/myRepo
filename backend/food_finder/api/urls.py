7d83e7ea9f8393d84e5bea7322a02f69c0cb8c5e
Ford Smith <fms34@case.edu>
2021-05-04 20:20:27 -0400

    Update urls for getting recommendations

--- backend/food_finder/api/urls.py
+++ backend/food_finder/api/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('restaurant/new', restaurant_data.restaurant_add, name='restaurant-new'),
     path('restaurant/<int:restaurant_id>', restaurant_data.restaurant_detail, name='restaurant-detail'),
 
-    path('user/update_preferences/liked', preference_data.update_preferences_liked),
+    path('user/recommendations/update/liked', preference_data.update_preferences_liked, name='update-preferences'),
+    path('user/recommendations/get', preference_data.get_recommendations),
 
     path('login/', user_accounts.login_user, name='login'),
     path('logout/', user_accounts.logout_user, name='logout'),
