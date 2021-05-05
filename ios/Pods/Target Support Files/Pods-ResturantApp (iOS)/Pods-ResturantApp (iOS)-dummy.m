7807ff7ee8125ea64653a28d8461877affd7575c
Michael <mhf25@case.edu>
2021-05-04 22:39:33 -0400

    michaelsucksatnamingthings

---
+++ ios/Pods/Target Support Files/Pods-ResturantApp (iOS)/Pods-ResturantApp (iOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ResturantApp__iOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_ResturantApp__iOS_
+@end
---
+++ ios/Shared/ResturantAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  ResturantAppApp.swift
+//  Shared
+//
+//  Created by Michael  on 3/30/21.
+//
+
+import SwiftUI
+@main
+struct ResturantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
